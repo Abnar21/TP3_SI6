27a366de3efa863737f009916a8e39ac28682858
Abnar21 <abnar21@gmail.com>
2017-02-09 13:55:07 +0100

    ajout de l'auteur dans create.php

--- public/create.php
+++ public/create.php
@@ -7,6 +7,10 @@
               <input type="text" class="form-control" id="titre" name="titre" placeholder="Titre du sujet">
             </div>
             <div class="form-group">
+                <label for="auteur">Auteur</label>
+                <input type="text" class="form-control" id="auteur" name="auteur" placeholder="Nom de l'auteur">
+            </div>
+            <div class="form-group">
               <label for="description">Description</label>
               <textarea class="form-control" rows="3" id="description" name="description" placeholder="Description du sujet"></textarea>
             </div>
